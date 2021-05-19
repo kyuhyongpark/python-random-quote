d39ef06fd9ce5649f143e73d823940d4fd218591
Kyu Hyong Park <kjp5774@psu.edu>
2021-05-19 15:09:22 -0400

    print two randomized quotes

--- get-quote.py
+++ get-quote.py
@@ -9,9 +9,15 @@ def primary():
 
   last = len(quotes) - 1
 
-  rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd1].rstrip())
+
+  rnd2 = random.randint(0, last)
+  while rnd1 == rnd2:
+    rnd2 = random.randint(0, last)
+
+  print(quotes[rnd2].rstrip())
 
 if __name__== "__main__":
   primary()
